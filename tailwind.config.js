e139f0de0d79eb6b09411bb741ddd9760a927a00
Theogene <igiranezatheogene4@gmail.com>
2025-09-07 16:47:04 +1100

    Porto page is in index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./test.html","./Lugx.html","./figma-page.html", "./scr**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html","./test.html","./Lugx.html","./figma-page.html","./porto.html", "./scr**/*.{js,ts,jsx,tsx}",],
   darkMode: 'class', // My additional class to enable dark mode
   theme: {
     extend: {
       boxShadow: {
         'all': '0 8px 10px rgba(0, 0, 0, 0.3)', // custom shadow on all sides
+      },
+      fontFamily: {
+        lato:['Lato', 'sans-serif'],
       },
     },
   },
